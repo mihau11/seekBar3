111080a7bddb512a1a115681924bc9a3bc8b8226
budzik12 <81246476+budzik12@users.noreply.github.com>
2023-02-14 09:36:41 +0100

    scaled zmniejsza

--- app/src/main/java/com/nkodem/seekbarocena/MainActivity.kt
+++ app/src/main/java/com/nkodem/seekbarocena/MainActivity.kt
@@ -16,34 +16,28 @@ import java.lang.StrictMath.round
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     @SuppressLint("MissingInflatedId")
-    fun changesizeimage(img:ImageView,h:Int,w:Int){
-        img.layoutParams.height=h
-        img.layoutParams.width=w
-        img.requestLayout()
-    }
     fun resizePhotoW(bitmap: Bitmap,prog:Int,dom:Int): Bitmap {
-        var newW=dom*prog*0.01
-        val b = Bitmap.createScaledBitmap(bitmap, round(newW).toInt(),bitmap.height, false)
+        var newW=prog*100.1
+        val b = Bitmap.createScaledBitmap(bitmap, round(newW).toInt(),bitmap.height, true)
         return b
     }
     fun resizePhotoH(bitmap: Bitmap,prog:Int,dom:Int): Bitmap {
-        var newH=dom*prog*0.01
-        val b = Bitmap.createScaledBitmap(bitmap, bitmap.width, round(newH).toInt(), false)
+        var newH=prog*100.1
+        val b = Bitmap.createScaledBitmap(bitmap,bitmap.width,round(newH).toInt(),true)
         return b
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val domWys = binding.imageView6.height
-        val domSze = binding.imageView6.width
         val view = binding.root
         setContentView(view)
+
         val piony = arrayListOf(binding.sb1pion,binding.sb2pion,binding.sb3pion)
         val poziomy = arrayListOf(binding.sb1poziom,binding.sb2poziom,binding.sb3poziom)
         val obrazki = arrayListOf(binding.imageView6,binding.imageView7,binding.imageView10)
-        val wysy = arrayListOf(0,0,0)
-        val szery = arrayListOf(0,0,0)
+        val domWys = 1
+        val domSze = 662
 
         for (x in 0..2) {
             piony[x].progress = 100
@@ -51,30 +45,19 @@ class MainActivity : AppCompatActivity() {
                 override fun onProgressChanged(seekBar: SeekBar, progres: Int, fromUser: Boolean) {
                     val progress=progres+1
                     var licznik2 = 0
-                    for (y in piony) {
-                        licznik2 += y.progress
-                    }
+                    for (y in piony){ licznik2 += y.progress}
                     binding.progressBar2.progress = licznik2 / 3
                     binding.textView.setText((licznik2/3).toString())
-                    //obrazki[x].layoutParams.width = round(domSze*progress*0.01).toInt()
-                    //obrazki[x].layoutParams.height = round(domWys*wysy[x]*0.01).toInt()
-                    //obrazki[x].requestLayout()
-                    //poziomy[x].progress=progress
-                    //changesizeimage(obrazki[x],round(domWys*progress*0.01).toInt(),round(domSze*szery[x]*0.01).toInt())
-                    //wysy[x]=progress
-                    obrazki[x].setImageBitmap(resizePhotoH(obrazki[x].drawToBitmap(),progres,domWys))
 
+                    obrazki[x].setImageBitmap(resizePhotoH(obrazki[x].drawToBitmap(),progress,domWys))
                 }
-
                 override fun onStartTrackingTouch(seekBar: SeekBar) {
                     // Write code to perform some action when touch is started.
                 }
-
                 override fun onStopTrackingTouch(seekBar: SeekBar) {
-                    // Write code to perform some action when touch is stopped.
                     Toast.makeText(
                         this@MainActivity,
-                        "Wysokość " + seekBar.progress + "%\n"+domWys,
+                        "Wysokość " + seekBar.progress + "%",
                         Toast.LENGTH_SHORT
                     ).show()
 
@@ -86,37 +69,21 @@ class MainActivity : AppCompatActivity() {
                 poziomy[x].setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
                     override fun onProgressChanged(seekBar: SeekBar, progres: Int, fromUser: Boolean) {
                         val progress = progres+1
-                        var licznik = 0
-                        for (y in poziomy) {
-                            licznik += y.progress
-                        }
+                        var licznik= 0
+                        for (y in poziomy){licznik += y.progress}
                         binding.progressBar.progress = licznik / 3
                         binding.progtext.setText((licznik/3).toString())
-                        
-                        //obrazki[x].layoutParams.height = round(domWys * progress *0.01).toInt()
-                        //obrazki[x].layoutParams.width = round(domSze*szery[x]*0.01).toInt()
-                        //obrazki[x].requestLayout()
-                        //piony[x].progress=progress-1
-                        //changesizeimage(obrazki[x],round(domWys*wysy[x]*0.01).toInt(),round(domSze*progress*0.01).toInt())
-                        //szery[x]=progress
-                        obrazki[x].setImageBitmap(resizePhotoW(obrazki[x].drawToBitmap(),progres,domSze))
-                    }
 
+                        obrazki[x].setImageBitmap(resizePhotoW(obrazki[x].drawToBitmap(),progress,domSze))
+                    }
                     override fun onStartTrackingTouch(seekBar: SeekBar) {
                         // Write code to perform some action when touch is started.
                     }
-
                     override fun onStopTrackingTouch(seekBar: SeekBar) {
                         // Write code to perform some action when touch is stopped.
                         Toast.makeText(
                             this@MainActivity,
-                            "Szerokość " + seekBar.progress + "%\n"+domSze  ,
+                            "Szerokość " + seekBar.progress + "%",
                             Toast.LENGTH_SHORT
                         ).show()
-
-                    }
-                })
-            }
-
-        }
-    }
+}})}}}
